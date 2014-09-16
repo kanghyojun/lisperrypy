3e6416cfb5f028c417eb2a1d5159e9548851eb24
Kang HyoJun <admire9@gmail.com>
2014-09-17 03:20:44 +0900

    Add tox

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='lisperrypy',
       version='0.0.1',
       author='Kang Hyojun',
       author_email='hyojun@admire.kr',
-      install_requires=[
-          'pytest>=2.3.5'
+      tests_require=[
+          'pytest>=2.3.5', 'tox>=1.7.2'
       ],
       packages=find_packages())
